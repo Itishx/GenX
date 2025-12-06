cc2deac5ce8c4a2b4a09729a3b1bcd9888aaf6b2
Itish Pande <itish.pande21@gmail.com>
2025-12-06 20:05:58 +0530

    Fix TypeScript error: change boolean to string for CORS header

--- api/chat.ts
+++ api/chat.ts
@@ -46,7 +46,7 @@ const formatCost = (cost: number): string => {
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     // Handle CORS
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
     res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
